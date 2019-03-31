9206fd60cc926e81f6aedbaed91740fcdead0ccb
Luzandro <andreas.wecer@gmail.com>
2019-03-31 08:31:47 +0200

    expand address range; translate non ascii chars

--- filter_address_files.py
+++ filter_address_files.py
@@ -9,13 +9,16 @@ from glob import glob
 import os
 import argparse
 from abbreviations import ABBREVIATIONS, NAMES
+import string
 
 LOCAL_OVERPASS_URL = "http://localhost/cgi-bin/overpass-api/interpreter"
 # TODO: addr:units
+# TODO: Non ASCII characters like Esterházy
 
 def get_existing_addresses(minlat, minlon, maxlat, maxlon):
     api = overpy.Overpass(url=OVERPASS_URL)
     query = 'nwr["addr:housenumber"](%s,%s,%s,%s);out center;' % (minlat, minlon, maxlat, maxlon)
+    #print(query)
     result = api.query(query)
 
     address_points = []
@@ -34,7 +37,12 @@ def get_existing_addresses(minlat, minlon, maxlat, maxlon):
             street = addr.tags["addr:place"]
         else:
             continue
-        street = normalize_streetname(street)
+        try:
+            street = normalize_streetname(street)
+        except ValueError:
+            error_file = open("invalid_characters.txt", "a+")
+            error_file.write("%s %s %s %s\n\n" % (street, addr.id, addr.__class__, str(addr.tags)))
+            error_file.close()
         housenumber = addr.tags["addr:housenumber"].lower()
 
         address = {'lat': addr.lat, 'lon': addr.lon}
@@ -45,6 +53,17 @@ def get_existing_addresses(minlat, minlon, maxlat, maxlon):
         elif "/" in housenumber:
             housenumber, address["unit"] = housenumber.split("/", 1)
         addresses[street][housenumber].append(address)
+
+        # add single addresses for (simple) address ranges to existing addresses
+        # i.e. for the existing address "49-51" add "49-51", "49" and "51"
+        if "-" in housenumber:
+            try:
+                lower_bound, upper_bound = [int(n) for n in housenumber.split("-", 1)]
+                for n in range(lower_bound, upper_bound+1, 2):
+                    addresses[street][n].append(address)
+            except ValueError:
+                pass
+
     return addresses
 
 def filter_address_files(list_of_filenames):
@@ -55,16 +74,24 @@ def filter_address_files(list_of_filenames):
         for alternative in alt_names[street]:
             for housenumber in alternative:
                 addresses[alternative][housenumber].append(addresses[street][housenumber])
+    overall_count = 0
+    filtered_count = 0
     for filename in list_of_filenames:
         if not "_filtered.osm" in filename or filename.startswith("NOTES_"):
             print(filename)
-            filter_address_file(filename, addresses)
+            overall_count_file, filtered_count_file = filter_address_file(filename, addresses)
+            overall_count += overall_count_file
+            filtered_count += filtered_count_file
+    return overall_count, filtered_count
 
 def filter_address_file(filename, addresses):
     tree = ET.parse(filename)
     root = tree.getroot()
+    overall_count = 0
+    filtered_count = 0
 
     for node in root.findall('node'):
+        overall_count += 1
         tags = {}
         for tag in node.findall('tag'):
             tags[tag.get("k")] = tag.get("v")
@@ -72,7 +99,12 @@ def filter_address_file(filename, addresses):
             street = tags["addr:street"]
         else:
             street = tags["addr:place"]
-        street = normalize_streetname(street)
+        try:
+            street = normalize_streetname(street)
+        except ValueError:
+            error_file = open("invalid_characters.txt", "a+")
+            error_file.write("%s %s\n" % (street, str(tags)))
+            error_file.close()
         housenumber = tags["addr:housenumber"].lower()
         if street in addresses and housenumber in addresses[street]:
             p1 = (float(node.get("lat")), float(node.get("lon")))
@@ -85,6 +117,7 @@ def filter_address_file(filename, addresses):
                         # if city is set and differs use higher threshold
                         continue
                     root.remove(node)
+                    filtered_count += 1
                     break
     directory, filename = os.path.split(os.path.abspath(filename))
     filtered_directory = "%s_filtered" % directory
@@ -98,10 +131,16 @@ def filter_address_file(filename, addresses):
     elif os.path.exists(filtered_path):
         os.remove(filtered_path)
         # TODO: directory empty?
+    return filtered_count, overall_count
 
 ''' strips whitespace/dash, ß->ss, ignore case '''
 def normalize_streetname(street, expand_abbreviations=True):
-    s = street.replace("ß", "ss").replace(" ", "").replace("-", "").lower()
+    valid_chars = string.ascii_letters + string.digits + "üäö.,()/;"
+    translation_table = str.maketrans("áčéěëèíóőřšúž", "aceeeeioorsuz")
+    s = street.replace("ß", "ss").replace(" ", "").replace("-", "").replace("'", "").lower()
+    s = s.replace("\xa0", "") # non breaking space
+    s = s.replace("&", "+")
+    s = s.translate(translation_table)
     if expand_abbreviations:
         if s.endswith("str.") or s.endswith("g."):
             s = s[:-1] + "asse"
@@ -125,6 +164,8 @@ def normalize_streetname(street, expand_abbreviations=True):
             #print(normalize_streetname.abbreviations)
         for key, value in normalize_streetname.abbreviations.items():
             s = s.replace(key, value)
+    if not all([char in valid_chars for char in s]):
+        raise ValueError("non ascii character found in street name: ", s)
     return s
 
 def get_alternative_streetnames(minlat, minlon, maxlat, maxlon, normalize_streetnames = True):
@@ -136,16 +177,26 @@ def get_alternative_streetnames(minlat, minlon, maxlat, maxlon, normalize_street
     api = overpy.Overpass(url=OVERPASS_URL)
     result = api.query('way[highway][alt_name][name](%s,%s,%s,%s);out;' % (minlat, minlon, maxlat, maxlon))
     for way in result.ways:
-        name = normalize(way.tags["name"])
-        alt_name = normalize(way.tags["alt_name"])
-        alt_names[name].add(alt_name)
-        alt_names[alt_name].add(name)
+        try:
+            name = normalize(way.tags["name"])
+            alt_name = normalize(way.tags["alt_name"])
+            alt_names[name].add(alt_name)
+            alt_names[alt_name].add(name)
+        except ValueError:
+            error_file = open("invalid_characters.txt", "a+")
+            error_file.write("%s %s (alt_name)\n\n" % (way.tags["alt_name"], way.tags["name"]))
+            error_file.close()
     result = api.query('way[highway][official_name](%s,%s,%s,%s);out;' % (minlat, minlon, maxlat, maxlon))
     for way in result.ways:
-        name = normalize(way.tags["name"])
-        official_name = normalize(way.tags["official_name"])
-        alt_names[name].add(official_name)
-        alt_names[official_name].add(name)
+        try:
+            name = normalize(way.tags["name"])
+            official_name = normalize(way.tags["official_name"])
+            alt_names[name].add(official_name)
+            alt_names[official_name].add(name)
+        except ValueError:
+            error_file = open("invalid_characters.txt", "a+")
+            error_file.write("%s %s (official_name)\n\n" % (way.tags["official_name"], way.tags["name"]))
+            error_file.close()
     return alt_names
 
 def get_distance(point1, point2):
@@ -196,7 +247,18 @@ if __name__ == '__main__':
     else:
         OVERPASS_URL = None
     if os.path.isdir(ARGS.directory[0]):
+        overall_count = 0
+        filtered_count = 0
         for root, dirs, files in os.walk(ARGS.directory[0]):
-            if len(dirs) == 0:
-                filter_address_files([os.path.join(root, f) for f in files])
+            dirs[:] = [d for d in dirs if not d.endswith("filtered")]
+            if len(dirs) == 0 and len(files) > 0:
+                print(root)
+                overall_count_dir, filtered_count_dir = filter_address_files([os.path.join(root, f) for f in files])
+                overall_count += overall_count_dir
+                filtered_count += filtered_count_dir
+                try:
+                    print("%d / %d nodes filtered (%d%%)" % (overall_count_dir,filtered_count_dir,(float(overall_count_dir)/filtered_count_dir*100)))
+                except ZeroDivisionError:
+                    pass
+        print("\nOverall:\n%d / %d nodes filtered (%d%%)" % (overall_count,filtered_count,(float(overall_count)/filtered_count*100)))
         
